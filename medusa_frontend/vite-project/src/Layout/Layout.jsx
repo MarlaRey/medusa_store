7445e44c8bc7e0d98fc107e50717631512687b50
MarlaRey <hejmarlenehej@gmail.com>
2024-05-03 14:04:24 +0200

    logo

--- medusa_frontend/vite-project/src/Layout/Layout.jsx
+++ medusa_frontend/vite-project/src/Layout/Layout.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa'; // Importer FaShoppingCart fra react-icons
-import styles from './Layout.module.scss'; // Importer SCSS-filen
+import { FaShoppingCart } from 'react-icons/fa';
+import styles from './Layout.module.scss';
+import logoImage from "../assets/img/logo/Coffee.png";
+import logobg from "../assets/img/logo/Ellipse 1.png";
+import logoText from "../assets/img/logo/Sunshine Coffee.png";
 
 export const Layout = () => {
-  return (<>
-    <div className={styles.navbar}>
-      <div>
-        <Link to="/home">
-          home
-        </Link>
+  return (
+    <>
+      <div className={styles.navbar}>
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/cart">Cart</Link></li>
+          </ul>
+        </nav>
+        <div className={styles.logoContainer}>
+          <Link to="/home">
+          <img src={logoText} alt="Logo" className={styles.logoText} />
+            <img src={logobg} alt="Logo Background" className={styles.logoBackground} />
+            <img src={logoImage} alt="Logo" className={styles.logo} />
+          </Link>
+        </div>
+        <div className={styles.icons}>
+          <FaShoppingCart />
+        </div>
       </div>
-      <nav>
-        <ul>
-          <li><Link to="/home">Home</Link></li>
-          <li><Link to="/products">Products</Link></li>
-          <li><Link to="/cart">Cart</Link></li>
-        </ul>
-      </nav>
-      <div className={styles.icons}>
-        <img src={FaShoppingCart} alt="Shopping Cart" /> {/* Brug FaShoppingCart som billedkilde */}
+     
+      <div className={styles.outletContainer}>
+        <Outlet />
       </div>
-
-    </div>
-          <Outlet />
-          <footer>footer</footer>
-          </>
+      <footer>footer</footer>
+    </>
   );
 };
